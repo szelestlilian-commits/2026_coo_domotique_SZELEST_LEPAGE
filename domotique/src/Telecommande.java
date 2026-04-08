2eaa3bcbe3d29eded05e851c279247cec35421f4
Lilian Szelest <szelest.lilian@gmail.com>
2026-04-08 17:20:53 +0200

    c fait

--- domotique/src/Telecommande.java
+++ domotique/src/Telecommande.java
@@ -1,6 +1,42 @@
-public class Telecommande {
-    private Lampe[] listeLampe;
+import java.util.*;
 
-    public Telecommande(Lampe liste){
+public class Telecommande {
+
+    private List<Lampe> lampes;
+
+    public Telecommande() {
+        lampes = new ArrayList<>();
+    }
+
+    public void ajouterLampe(Lampe l) {
+        lampes.add(l);
+    }
+
+    public void activerLampe(int i) {
+        if (i < 0 || i >= lampes.size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        lampes.get(i).allumer();
+    }
+
+    public void desactiverLampe(int i) {
+        if (i < 0 || i >= lampes.size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        lampes.get(i).eteindre();
+    }
+
+    public void activerTout() {
+        for (Lampe l : lampes) {
+            l.allumer();
+        }
+    }
+
+    public String toString() {
+        String res = "";
+        for (int i = 0; i < lampes.size(); i++) {
+            res += i + " : " + lampes.get(i) + "\n";
+        }
+        return res;
     }
 }
