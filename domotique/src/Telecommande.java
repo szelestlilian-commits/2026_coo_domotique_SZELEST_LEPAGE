dcfc527e8dcca0400ff40830019dfd192158f53e
LEPAGE Thomas <thomaslegithub@gmail.com>
2026-04-08 16:35:35 +0200

    Squelette écrit.

--- domotique/src/Telecommande.java
+++ domotique/src/Telecommande.java
@@ -1,6 +1,31 @@
-public class Telecommande {
-    private Lampe[] listeLampe;
+import java.util.ArrayList;
 
-    public Telecommande(Lampe liste){
+public class Telecommande {
+    private ArrayList<Lampe> listeLampe;
+
+    public Telecommande(Lampe[] liste) {
+        for (int i = 0; i < liste.length; i++) {
+            this.listeLampe.add(liste[i]);
+        }
+    }
+
+    public void ajouterLampe(Lampe l) {
+        throw new Error("code non ecrit");
+    }
+
+    public void activerLampe(int i) {
+        throw new Error("code non ecrit");
+    }
+
+    public void desactiverLampe(int i) {
+        throw new Error("code non ecrit");
+    }
+
+    public void activerTout() {
+        throw new Error("code non ecrit");
+    }
+
+    public String toString() {
+        throw new Error("code non ecrit");
     }
 }
