337e91ac9feab52d04a2d9b61cb103719b32c137
LEPAGE Thomas <thomaslegithub@gmail.com>
2026-04-27 08:55:40 +0200

    Correction d'une erreur de frappe

--- domotique/src/Main.java
+++ domotique/src/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		// la telecommande vide
 		Telecommande t = new Telecommande();
 
